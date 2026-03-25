23a046c80d3ed9d5c51c70a7fffb11fe2795bb27
Philipp David <pd@3b.pm>
2026-03-25 18:36:50 +0100

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs [
+          "aarch64-darwin"
+          "aarch64-linux"
+          "x86_64-linux"
+        ] (system: function nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (
+        pkgs: with pkgs; {
+          default = mkShell {
+            nativeBuildInputs = [
+              nodejs_25
+            ];
+          };
+        }
+      );
+
+      packages = forAllSystems (pkgs: {
+        default = pkgs.buildNpmPackage {
+          pname = "csp-hash-from-html";
+          version = "0.3.1";
+          src = ./.;
+          npmDepsHash = "sha256-J86KddxnWe3ouFKpmw6ihJ/IFQgo/Z/2tUkcXLRjamg=";
+        };
+      });
+    };
+}
